5219a070a095b12373b7348727c6b6b7fd352956
alizayan684 <alizayan684@gmail.com>
2023-09-17 18:13:48 +0300

    last_update

--- main.py
+++ main.py
@@ -1,201 +1,238 @@
 import csv
+import math
 import random
 from queue import PriorityQueue
 
 import numpy
 
+# define needed constants ##############################################################################################
+OBSTACLE_FILE_PATH = "BEST/obstacles.csv"  # the path to the obstacle definition csv file
+NODES_FILE_PATH = "BEST/nodes.csv"  # the path to write a csv file of generated nodes
+EDGES_FILE_PATH = "BEST/edges.csv"  # the path to write a csv file of generated edges
+PATH_FILE_PATH = "BEST/path.csv"  # the path to write a csv file of the node path
+GRAPH_MIN = -0.5  # minimum for generating a random sample point
+GRAPH_MAX = 0.5  # maximum for generating a random sample point
+MAX_LOOP_COUNT = 500  # defines the maximum number of attempts to find a path
+SAMPLE_STEP_SIZE = 0.2  # defines the maximum step size to take when generating a new node
+MIN_GOAL_DISTANCE = 0.001  # defines the error tolerance for checking if the goal has been reached
+OBSTACLE_BUFFER = 0.02  # defines a small buffer distance to add to each obstacle for a better screenshot
 
-# output of RRT algorithm ##############################################################################################
-def trace_path(goal_node):
-    path = []
-    current_node = goal_node
-    while current_node.parent:
-        path.append([round(current_node.x, 2), round(current_node.y, 2)])
-        current_node = current_node.parent
-    path.append([current_node.x, current_node.y])
-    path.reverse()
-    return path
 
-
+########################################################################################################################
 # Read in obstacles from csv file and store in list ####################################################################
+
 def read_obstacles():
-    with open('BEST/obstacles.csv') as csvfile:
-        reader = csv.reader(csvfile)
-        obstacles = list(reader)
-    csvfile.close()
+    with open(OBSTACLE_FILE_PATH) as csv_file:
+        csv_reader = csv.reader(filter(lambda row_: row_[0] != '#', csv_file))
+        obstacles = list(csv_reader)
+    csv_file.close()
     return obstacles
 
 
-# make sample point function for RRT ###################################################################################
-def sample_nodes():
-    samples = PriorityQueue()  # initialize priority queue
-    for samp_num in range(8):  # generate 8 random points
-        x = random.uniform(-0.6, 0.6)
-        y = random.uniform(-0.6, 0.6)
-        point = [round(x, 2), round(y, 2)]
-        samples.put(Node(point[0], point[1]))
-    return samples
+# writing output in csv files ##########################################################################################
+def write_nodes_csv(node_csv_file_path, nodes):
+    lines = []
+    for node in nodes:
+        lines.append(f"{node.id}, {node.x:.4f}, {node.y:.4f}, 0\n")
+
+    with open(node_csv_file_path, "w") as csv_file:
+        csv_file.writelines(lines)
 
 
-# make unit vector function for RRT ####################################################################################
-def make_unit_vector(start_Node, end_Point):  # make unit vector for steering and obstacle checking
-    if start_Node.x == end_Point[0] and start_Node.y == end_Point[1]:
-        return [0, 0]
-    vector = [end_Point[0] - start_Node.x, end_Point[1] - start_Node.y]
-    vector = [(end_Point[0] - start_Node.x) / (numpy.sqrt(vector[0] ** 2 + vector[1] ** 2)),
-              (end_Point[1] - start_Node.y) / (numpy.sqrt(vector[0] ** 2 + vector[1] ** 2))]
+# write a list of edges to a csv file
+def write_edges_csv(edges_csv_file_path, edges):
+    lines = []
+    for edge in edges:
+        lines.append(f"{edge.node_id_1}, {edge.node_id_2}, {edge.cost}\n")
 
-    return vector
+    with open(edges_csv_file_path, "w") as csv_file:
+        csv_file.writelines(lines)
+
+
+# walk up the node tree and write the path to the goal
+def write_path_csv(path_csv_file, goal_node):
+    result = []
+    current_node = goal_node
+    while current_node:
+        result.append(current_node.id)
+        current_node = current_node.parent
+
+    result.reverse()
+    num_results = len(result)
+    with open(path_csv_file, "w") as csv_file:
+        for i in range(0, num_results):
+            csv_file.write(str(result[i]))
+            if i + 1 < num_results:
+                csv_file.write(",")
+
+
+########################################################################################################################
+def no_intersection(x1, y1, x2, y2, cx, cy, circle_radius):
+    # Convert points to numpy arrays for easier math
+    p1 = numpy.array([x1, y1])
+    p2 = numpy.array([x2, y2])
+
+    # Compute directional vector of line
+    dp = p2 - p1
+    magnitude = numpy.linalg.norm(dp)
+    unit_vector = dp / magnitude
+    for i in range(21):
+        new_point = p1 + unit_vector * (i / 20) * magnitude
+        if ((new_point[0] - cx) ** 2 + (new_point[1] - cy) ** 2) ** 0.5 <= (
+                circle_radius):
+            return False
+
+    return True
 
 
 # Node class ###########################################################################################################
 class Node:
     def __init__(self, x, y):
+        self.id = 0
         self.x = x
         self.y = y
         self.parent = None
         self.children = []
-        # calculate heuristic for priority queue (distance to goal) for sampling
-        self.heuristic = numpy.sqrt((x - goal[0]) ** 2 + (y - goal[1]) ** 2)
+        self.heuristic = ((GRAPH_MAX - x) ** 2 + (GRAPH_MAX - y) ** 2) ** 0.5
 
-    def __lt__(self, other):  # less than function for priority queue to compare sample points
+    def __lt__(self, other):
         return self.heuristic < other.heuristic
+
+
+# defines a class representing an edge between two nodes of the graph
+class Edge:
+    def __init__(self, node_id_1, node_id_2):
+        self.node_id_1 = node_id_1
+        self.node_id_2 = node_id_2
+        self.cost = 0
 
 
 # RRT class ############################################################################################################
 class RRT:
-    def __init__(self, start_point, goal_point, iterations, obstacles, max_length):
-        self.START = Node(start_point[0], start_point[1])
-        self.GOAL = Node(goal_point[0], goal_point[1])
-        self.nearest_node = None
-        self.iterations = iterations
-        self.grid = obstacles
-        self.step_size = max_length
-        self.path = []
-        self.nearest_distance = 10000
+    def __init__(self):
+        self.node_count = 1
+        self.nodes = []
+        self.edges = []
 
-    def add_child(self, x, y):  # add child to nearest node
-        if x == self.GOAL.x and y == self.GOAL.y:
-            self.GOAL.parent = self.nearest_node
-            self.nearest_node.children.append(self.GOAL)
+    # the tree is responsible for ensuring that each added node has a unique ID
+    def add_node(self, node):
+        node.id = self.node_count
+        self.nodes.append(node)
+        self.node_count = self.node_count + 1
+        return node.id
+
+    def add_edge(self, edge):
+        self.edges.append(edge)
+
+
+########################################################################################################################
+random_points = []
+for i in range(-50, 51):
+    for j in range(-50, 51):
+        random_points.append([i / 100, j / 100])
+
+
+def get_sample_point():
+    qsamples = PriorityQueue()
+    for i in range(10):
+        point = random.choice(random_points)
+        sample_node = Node(point[0], point[1])
+        qsamples.put(sample_node)
+    chosen_node = qsamples.get()
+    return [chosen_node.x, chosen_node.y]
+
+
+def check_motion_is_collision_free(obstacle_list, start_x, start_y, end_x, end_y):
+    for obstacle in obstacle_list:
+        if no_intersection(start_x, start_y, end_x, end_y, float(obstacle[0]), float(obstacle[1]),
+                           float(obstacle[2]) / 2.0 + OBSTACLE_BUFFER):
+            continue
         else:
-            new_node = Node(x, y)
-            new_node.parent = self.nearest_node
-            self.nearest_node.children.append(new_node)
+            return False
+    return True
 
-    def steer_towards(self, start_node, end_point):  # steer towards random point
-        vector = make_unit_vector(start_node, end_point)
-        vector = [vector[0] * self.step_size, vector[1] * self.step_size]
-        new_Point = [start_node.x + vector[0], start_node.y + vector[1]]
-        if new_Point[0] > 0.5:  # check if new point x is out of bounds
-            new_Point[0] = 0.5
 
-        if new_Point[0] < -0.5:
-            new_Point[0] = -0.5
+def get_closest_tree_node(tree, x, y):
+    min_distance = math.inf
+    closest_node = None
+    for node in tree.nodes:
+        distance = ((x - node.x) ** 2 + (y - node.y) ** 2) ** 0.5
+        if distance < min_distance:
+            min_distance = distance
+            closest_node = node
+    return closest_node
 
-        if new_Point[1] > 0.5:  # check if new point y is out of bounds
-            new_Point[1] = 0.5
 
-        if new_Point[1] < -0.5:
-            new_Point[1] = -0.5
+def local_planner(start_x, start_y, target_x, target_y, step_size):
+    # find the normalized vector from the start point to the target
+    start = numpy.array([start_x, start_y])
+    target = numpy.array([target_x, target_y])
+    direction = target - start
 
-        return new_Point
+    # if the movement is greater than step_size, reduce it
+    magnitude = numpy.linalg.norm(direction)
+    if magnitude > step_size:
+        direction = direction / magnitude
+        direction = direction * step_size
 
-    def obstacle_found(self, nearest_Node, new_Point):  # check if obstacle is in path
-        unit_vector = make_unit_vector(nearest_Node, new_Point)  # make unit vector
-        test_point = [nearest_Node.x, nearest_Node.y]
-        while self.distance(test_point, new_Point) > 0.05:  # check if obstacle is in path
-            test_point[0] += unit_vector[0] * 0.05  # check every 0.05m
-            test_point[1] += unit_vector[1] * 0.05  # check every 0.05m
-            if self.is_point_in_obstacle(test_point):
-                return True
-        if self.is_point_in_obstacle(new_Point):
-            return True
-        return False
-
-    def find_nearest_node(self, root, point):  # find nearest node to random point
-        if not root:
-            return
-        root_point = [root.x, root.y]
-        distance = self.distance(root_point, point)
-        if distance < self.nearest_distance:
-            self.nearest_node = root
-            self.nearest_distance = distance
-        if len(root.children):
-            for child in root.children:
-                self.find_nearest_node(child, point)
-        return self.nearest_node
-
-    def distance(self, point1, point2):  # calculate distance between two points
-        return numpy.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
-
-    def goal_reached(self, goal_node):  # check if goal is reached
-        if self.nearest_node is not None:
-            for child in self.nearest_node.children:
-                if ((child.x - goal_node.x) ** 2 + (
-                        child.y - goal_node.y) ** 2) <= 0.33:  # check if goal is reached within 0.37m
-                    self.GOAL.parent = child
-                    child.children.append(self.GOAL)
-                    return True
-        return False
-
-    def reset_nearest_distance(self):  # reset nearest distance to a large number
-        self.nearest_distance = 10000
-
-    def is_point_in_obstacle(self, point):  # check if point is in obstacle
-        for obstacle in self.grid:
-            if ((point[0] - float(obstacle[0])) ** 2 + (point[1] - float(obstacle[1])) ** 2) <= float(
-                    obstacle[2]) ** 2:  # check if point is in obstacle
-                return True
-        return False
+    # return the adjusted target point
+    new = start + direction
+    return new[0], new[1]
 
 
 if __name__ == '__main__':
-    # inputs ##################################################
-    path_points = []
-    start = [-0.5, -0.5]
-    goal = [0.5, 0.5]
-    ITERATIONS = 1000000
-    grid_obstacles = read_obstacles()
-    step_size = 0.3
-    ###########################################################
-    rrt = RRT(start, goal, ITERATIONS, grid_obstacles, step_size)  # initialize RRT
-    random_nodes = sample_nodes()  # initialize random nodes
-    counter = 8  # counter for random nodes
-    for i in range(ITERATIONS):
-        counter -= 1
-        if counter == 0:  # get new random nodes every 8 iterations
-            counter = 8
-            random_nodes = sample_nodes()
-        rand_node = random_nodes.get()
-        random_point = [rand_node.x, rand_node.y]
-        nearest_node = rrt.find_nearest_node(rrt.START, random_point)
-        new_point = rrt.steer_towards(nearest_node, random_point)
-        if not rrt.obstacle_found(nearest_node, new_point):
-            rrt.add_child(new_point[0], new_point[1])
-            random_nodes = sample_nodes()
-            rrt.reset_nearest_distance()
-        if rrt.goal_reached(rrt.GOAL):
-            path_points = trace_path(rrt.GOAL)
-            break
-        rrt.reset_nearest_distance()
-    # end of RRT algorithm #########################################
-    print(path_points)
-    # write to nodes file ##########################################
-    with open("BEST/nodes.csv", 'w') as n:
-        writer = csv.writer(n)
-        for i in range(len(path_points)):
-            writer.writerow([i + 1, path_points[i][0], path_points[i][1]])
-    # write to path file ##########################################
-    li = []
-    for i in range(len(path_points)):
-        li.append(i + 1)
-    with open("BEST/path.csv", 'w') as p:
-        writer = csv.writer(p)
-        writer.writerow(li)
-    # write to edges file ##########################################
-    with open("BEST/edges.csv", "w") as e:
-        writer = csv.writer(e)
-        for i in range(len(path_points) - 1):
-            writer.writerow([i + 1, i + 2, 0.3])
-    # END OF PROGRAM ###################################################################################################
+    # read in the obstacles
+    obstacles = read_obstacles()
+
+    # create the RRT tree
+    rrt = RRT()
+
+    # create the start and goal nodes
+    start_node = Node(-0.5, -0.5)
+    goal_node = Node(0.5, 0.5)
+
+    # add the start node to the tree
+    rrt.add_node(start_node)
+
+    # loop until the goal is reached or the maximum number of iterations is reached
+    goal_reached = False
+    loop_count = 0
+    while not goal_reached and loop_count < MAX_LOOP_COUNT:
+        # get a random sample point
+        if loop_count % 10 == 0:
+            sample_point = [0.5, 0.5]
+        else:
+            sample_point = get_sample_point()
+
+        # find the closest node in the tree to the sample point
+        closest_node = get_closest_tree_node(rrt, sample_point[0], sample_point[1])
+
+        # find the new point to add to the tree
+        new_point = local_planner(closest_node.x, closest_node.y, sample_point[0], sample_point[1], SAMPLE_STEP_SIZE)
+
+        # check if the new point is collision free
+        if check_motion_is_collision_free(obstacles, closest_node.x, closest_node.y, new_point[0], new_point[1]):
+            # create a new node and add it to the tree
+            new_node = Node(new_point[0], new_point[1])
+            new_node.parent = closest_node
+            closest_node.children.append(new_node)
+            rrt.add_node(new_node)
+
+            # create a new edge and add it to the tree
+            new_edge = Edge(closest_node.id, new_node.id)
+            rrt.add_edge(new_edge)
+            print(f"Added node {new_node.id} with parent {closest_node.id} and edge cost {new_edge.cost}")
+
+            # check if the goal has been reached
+            if ((new_node.x - goal_node.x) ** 2 + (new_node.y - goal_node.y) ** 2) ** .5 < MIN_GOAL_DISTANCE:
+                goal_reached = True
+                # write the nodes and edges to csv files
+                write_nodes_csv(NODES_FILE_PATH, rrt.nodes)
+                write_edges_csv(EDGES_FILE_PATH, rrt.edges)
+                write_path_csv(PATH_FILE_PATH, new_node)
+                print("Goal Reached")
+
+        # increment the loop counter
+        loop_count = loop_count + 1
+
+# END OF PROGRAM ###################################################################################################
